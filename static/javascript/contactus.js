7855543721ae3bf9dd53842888e14fd1152018f9
AleenaMariya104 <aleena.sebastian@msds.christuniversity.in>
2024-02-03 11:52:07 +0530

    aleena push

---
+++ static/javascript/contactus.js
@@ -0,0 +1,10 @@
+function submitForm() {
+        
+    document.getElementById('success-message').style.display = 'block';
+
+   
+    document.querySelector('form.container').style.display = 'none';
+
+   
+    return false;
+}
